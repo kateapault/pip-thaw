945e26f8fbabbb92a0cb24ea997957fc8f770b2c
github username <kateapault@gmail.com>
2020-06-16 11:51:22 -0400

    deleted example.py and added test requirements.txt file to gitignore

--- example.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description="A test program")
-parser.add_argument("add", nargs = "*", metavar="num", type=int, help="All the number separated by spaces will be aded")
-
-args = parser.parse_args()
-
-if len(args.add) != 0:
-    print(sum(args.add))
