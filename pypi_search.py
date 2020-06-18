3d1423cc101dc80118f05901d7ac60153d2ea0b0
github username <kateapault@gmail.com>
2020-06-18 14:49:06 -0400

    replaced pip list --outdated with direct search for latest version for each package in requirements.txt

---
+++ pypi_search.py
@@ -0,0 +1,24 @@
+from urllib import request
+
+def get_latest_version(package_name):
+    url = f"https://pypi.org/project/{package_name}/"
+    result = request.urlopen(url)
+    binary_data = result.read()
+    data = binary_data.decode('utf-8')
+    
+    fulltitle = hacky_parse_for_package_title(data)
+    name, version = fulltitle.split(' ')
+    return version
+
+
+def hacky_parse_for_package_title(html_string):
+    classname_start = html_string.find("package-header__name")
+    
+    inner_start = html_string[classname_start:].find(">")
+    text_start = inner_start + classname_start + 1
+    
+    inner_end = html_string[classname_start:].find("<")
+    text_end = inner_end + classname_start
+    
+    return html_string[text_start:text_end].strip()
+
--- thaw.py
+++ thaw.py
@@ -2,7 +2,7 @@
 """
 pip-thaw updates versions in your requirements.txt file that are out of date.
 
-You can choose to apply major changes (eg v1.x -> v2.x), minor changes (eg v1.0 -> v1.3), or all changes.
+You can choose to apply all changes or select from major/minor/micro.
 
 Requires ``pip`` Version 9 or higher!
 
@@ -11,11 +11,22 @@ Installation::
 
 Usage::
     pip-thaw -h
+    
+    --f --files             lists files that will have errors?
+    --u --update            takes arguments minor/major/micro/all, can take multiple
+    OR -----------> 
+    --a --all               apply all updates
+    --j --major             apply major updates
+    --n --minor             apply minor updates
+    --c --micro             apply micro updates
+    --mr --major-minor      apply major and minor updates
+    --mi --minor-micro      apply minor and micro updates
 """
 import argparse
 import subprocess
 import sys
 
+from pypi_search import get_latest_version
 
 # --------------------------------------------
 # SETTINGS -----------------------------------
@@ -23,7 +34,7 @@ import sys
 
 pip_cmd = "pip3"
 
-check_cmd = "pip3 list --outdated"
+check_cmd = "pip3 list --local --outdated"
 
 # err = sys.stderr.write
 # out = sys.stdout.write
@@ -36,7 +47,7 @@ check_cmd = "pip3 list --outdated"
 def dictify_pip_list(pip_stdout):
     pip_string = pip_stdout.decode('utf-8')
     pip_arr = pip_string.split()
-    
+
     pip_list_dict = {}
     i = 8
     while i < len(pip_arr):
@@ -49,6 +60,7 @@ def dictify_pip_list(pip_stdout):
             library_info["latest"] = pip_arr[i]
         else:
             update_scale = version_change_scale(library_info["current"], library_info["latest"])
+            print(f"{library_name}: version {library_info['current']} to version {library_info['latest']} => {update_scale} update")
             library_info["scale"] = update_scale
             pip_list_dict[library_name] = library_info
         i += 1
@@ -66,7 +78,6 @@ def version_change_scale(old_version_string, new_version_string):
     minor -> 1.4 to 1.7
     micro ->  1.4.3 to 1.4.4
     """
-    
     old_version = old_version_string.split('.')
     new_version = new_version_string.split('.')
     
@@ -92,37 +103,59 @@ def main():
     except FileNotFoundError:
         print("No requirements file found - please run thaw in the top level of your project")
     
-    cmd_list = check_cmd.split(' ')
-    # print(f"cmd_list: {cmd_list}")
-    process = subprocess.Popen(cmd_list,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    outdated_versions_dict = dictify_pip_list(stdout)
+    # cmd_list = check_cmd.split(' ')
     
-    # print(outdated_versions_dict)
+    # process = subprocess.Popen(cmd_list,
+    #                            stdout=subprocess.PIPE,
+    #                            stderr=subprocess.PIPE)
+    # stdout, stderr = process.communicate()
+    # outdated_versions_dict = dictify_pip_list(stdout)
 
-    commented_requirements_text = ""
-    
+    # commented_requirements_text = ""
+    scales = {
+        "major": {
+            "count":0,
+            "libraries":[],
+        },
+        "minor": {
+            "count":0,
+            "libraries":[],
+        },
+        "micro": {
+            "count":0,
+            "libraries":[],
+        }
+    }
     for line in requirements:
-        library = line.split("==")[0]
-        if library in outdated_versions_dict:
-            printline = f"{library} - {outdated_versions_dict[library]['scale']} update required"
-            print(printline)
-            latest = outdated_versions_dict[library]["latest"]
-            if len(line) <= 50:
-                new_commented_line = f"{line.strip():<50}\t#{latest}\n"
-            else:
-                new_commented_line = f"{line.strip()}\t#{latest}\n"
-            commented_requirements_text += new_commented_line
-        elif line.strip() != "-e .":
-            commented_requirements_text += line
-            
+
+        if "==" in line:
+            library, current_version = line.strip().split("==")
+            latest_version = get_latest_version(library)
+            scale = version_change_scale(current_version,latest_version)
+            if scale:
+                scales[scale]["count"] += 1
+            # print(f"{library:<20} | {scale} | {current_version:<10} | {latest_version:<10}")
+
+        # if current_version != latest_version:
+        #     printline = f"{library} - {outdated_versions_dict[library]['scale']} update required"
+        #     print(printline)
+        #     latest = outdated_versions_dict[library]["latest"]
+        #     if len(line) <= 50:
+        #         new_commented_line = f"{line.strip():<50}\t#{latest}\n"
+        #     else:
+        #         new_commented_line = f"{line.strip()}\t#{latest}\n"
+        #     commented_requirements_text += new_commented_line
+        # elif line.strip() != "-e .":
+        #     commented_requirements_text += line
+    print(f"{scales['major']['count'] + scales['minor']['count'] + scales['micro']['count']} TOTAL updates")
+    print(f"{scales['major']['count']} MAJOR updates")
+    print(f"{scales['minor']['count']} MINOR updates")
+    print(f"{scales['micro']['count']} MICRO updates")
     requirements.close()
 
-    requirements = open("requirements.txt","w")
-    requirements.write(commented_requirements_text)
-    requirements.close()
+    # requirements = open("requirements.txt","w")
+    # requirements.write(commented_requirements_text)
+    # requirements.close()
     
 if __name__ == "__main__":
     main()
